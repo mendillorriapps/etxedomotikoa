e86fe386691bced98c8dd7bfb7a68988bac369cd
Mendillorri Apps <mendillorriapps@educacion.navarra.es>
2018-04-11 13:19:29 +0200

    helbideak route ongi

--- api/api.py
+++ api/api.py
@@ -1,37 +1,40 @@
 from flask import Flask, render_template, request
 app = Flask(__name__,static_url_path='/static')
 
-@app.route (/tenperatura/<T>)
+@app.route ('/tenperatura/<T>')
 def jasoTemperatura(T):
-    pass
+    print(T)
+    return 'OK'
 
-@app.route (/argia/<argi>)
+@app.route ('/argia/<argi>')
 def jasoArgia(argi):
-    pass
+    print(argi)
+    return 'OK'
 
-@app.route (/mugimendua/<mug>)
+@app.route ('/mugimendua/<mug>')
 def jasoMugimendua(mug):
-    pass
+    print(mug)
+    return 'OK'
 
-@app.route (/ponpa/<ponpa>)
+@app.route ('/ponpa/')
 def bidaliPonpa(ponpa):
-    return 1
+    return '1'
 
-@app.route (/bozgoragailua/<bozg>)
+@app.route ('/bozgoragailua/')
 def bidaliBozg(bozg):
-    return 1
+    return '1'
 
-@app.route (/ledgorria/<ĺedgorria>)
-def bidaliledgorria(ledgoriia)
-    return 1
+@app.route ('/ledgorria/')
+def bidaliledgorria():
+    return '1'
 
-@app.route (/ledurdina/<ĺedurdina>)
-def bidaliledurdina(ledurdina)
-    return 1
+@app.route ('/ledurdina/')
+def bidaliledurdina():
+    return '1'
 
-@app.route (/ledtxuria/<ĺedtxuria>)
-def bidaliledtxuria(ledtxuria)
-    return 1
+@app.route ('/ledtxuria/')
+def bidaliledtxuria():
+    return '1'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
