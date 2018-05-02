a4181b3d6303e9429dca0f1e3dd11e022e6aad23
Mendillorri Apps <mendillorriapps@educacion.navarra.es>
2018-05-02 13:09:40 +0200

    aldaketa batzuk

--- api/api.py
+++ api/api.py
@@ -1,12 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,update
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 from datetime import datetime
 
 from datuak import Tenperatura
+
+from datuak import Argia
+
+from datuak import Mugimendua
+
+from datuak import Ponpa
+
+from datuak import Konfigurazioa
+
+#from datuak import Ledtxuria
+
+tmax=24
+ar=500
 
 eng = create_engine('sqlite:///test.db')
 
@@ -26,39 +39,124 @@ def jasoTemperatura(T):
     t1 = Tenperatura(tenperatura=T,ordua=datetime.now())
     ses.add(t1)
     ses.commit()
-    return 'OK'
+    return jsonify (
+        status = "OK"
+    )
 
 @app.route ('/argia/<argi>')
 def jasoArgia(argi):
     print(argi)
-    return 'OK'
+    a1= Argia(argia=argi,ordua=datetime.now())
+    ses.add(a1)
+    ses.commit()
+    return jsonify (
+        status = "OK"
+    )
 
 @app.route ('/mugimendua/<mug>')
 def jasoMugimendua(mug):
     print(mug)
-    return 'OK'
+    m1= Mugimendua(mugimendua=mug,ordua=datetime.now())
+    ses.add(m1)
+    ses.commit()
+    return jsonify (
+        status = "OK"
+    )
 
 @app.route ('/ponpa/')
 def bidaliPonpa():
     return '1'
 
+def bozgo():
+    rs = ses.query(Mugimendua).order_by(Mugimendua.ordua.desc()).first()
+    if rs.mugimendua == 1:
+        return '1'
+    else:
+        return '0'
+
 @app.route ('/bozgoragailua/')
-    return '1'
+def bidaliBozg():
+    return jsonify(    
+      bozg = bozgo()
+      )
+
+
+
+
+def lgorria():
+    rs = ses.query(Tenperatura).order_by(Tenperatura.ordua.desc()).first()
+    tmin = ses.query(Konfigurazioa,giltza="tmin")
+    if rs.tenperatura<tmin:
+        return '1'
+    else:
+        return '0'
 
 @app.route ('/ledgorria/')
 def bidaliledgorria():
-    rs = ses.query(Tenperatura).order_by(Tenperatura.ordua.desc()).first()
+    return jsonify(    
+        ledgorria = lgorria()
+    )
 
-    print(rs.tenperatura)
-    return '1'
+
+
+def lurdina():
+    rs = ses.query(Tenperatura).order_by(Tenperatura.ordua.desc()).first()
+    if rs.tenperatura>tmax:
+        return '1'
+    else:
+        return '0'
 
 @app.route ('/ledurdina/')
 def bidaliledurdina():
-    return '1'
+    return jsonify(
+        ledurdina = lurdina()
+    )
+
+
+
+def ltxuria():
+    rs = ses.query(Argia).order_by(Argia.ordua.desc()).first()
+    if rs.argia < ar:
+        return '1'
+    else:
+        return '0'
 
 @app.route ('/ledtxuria/')
-def bidaliledtxuria():
-    return '1'
+def bidaliledtxuria():    
+    return jsonify(
+        ledtxuria = ltxuria()
+    )
+
+
+
+@app.route ('/guztia/')
+def bidaliguztia():    
+    return jsonify(
+        ledtxuria = ltxuria(),
+        ledurdina = lurdina(),
+        bozg = bozgo(),
+        ledgorria = lgorria()  
+    )
+
+
+@app.route ('/tmin/<T>')
+def eguneratuTmin(T):
+    rs = ses.query(Konfigurazioa).filter(Konfigurazioa.giltza=="tmin").first()
+    rs.balioa=T
+    ses.commit()
+    return jsonify (
+        status = "OK"
+    )
+
+@app.route ('/tmax/<T>')
+def eguneratuTmin(T):
+    rs = ses.query(Konfigurazioa).filter(Konfigurazioa.giltza=="tmax").first()
+    rs.balioa=T
+    ses.commit()
+    return jsonify (
+        status = "OK"
+    )
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8080)
--- api/datuak.py
+++ api/datuak.py
@@ -13,5 +13,29 @@ class Tenperatura(Base):
     ordua= Column(DateTime, nullable=False)
         
 
+class Argia(Base):
+    __tablename__ = 'argia'
+    id = Column(Integer, primary_key=True)
+    argia= Column(Float, nullable=False)
+    ordua= Column(DateTime, nullable=False)
+
+class Mugimendua(Base):
+    __tablename__ = 'mugimendua'
+    id = Column(Integer, primary_key=True)
+    mugimendua= Column(Float, nullable=False)
+    ordua= Column(DateTime, nullable=False)
+
+class Ponpa(Base):
+    __tablename__ = 'ponpa'
+    id = Column(Integer, primary_key=True)
+    ponpa= Column(Float, nullable=False)
+    ordua= Column(DateTime, nullable=False)
+
+class Konfigurazioa(Base):
+    __tablename__ = 'konfigurazioa'
+    giltza = Column(String, primary_key=True)
+    balioa= Column(Float, nullable=False)
+    
+
 
 
